c690c9d06dbc7c6f2ced1bf741d240048c9b3262
preeti <preetikushwaha0110@gmail.com>
2024-10-17 18:51:33 +0530

    api created

---
+++ Backend/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const acccessToken = "pat-na1-e60a534a-9e18-4417-a962-b0d5c5e15679";
+app.get("/", async (req, res) => {
+    res.json("server is running")
+})
+app.get("/contacts", async (req, res) => {
+  const { offset = 0 } = req.query;
+  try {
+    const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/contacts?limit=100&offset=${offset}`, {
+      headers: {
+        Authorization: `Bearer ${acccessToken}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/contacts", async (req, res) => {
+  const { email, phone } = req.body;
+  try {
+    const response = await axios.post(`https://api.hubapi.com/crm/v3/objects/contacts`, {
+      properties: { email, phone },
+    }, {
+      headers: {
+        Authorization: `Bearer ${acccessToken}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/companies", async (req, res) => {
+  const { offset = 0 } = req.query; 
+  try {
+    const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/companies?limit=100&offset=${offset}`, {
+      headers: {
+        Authorization: `Bearer ${acccessToken}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT =8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ Backend/routes/companyRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getCompanies } = require('../controllers/companyController');
+const router = express.Router();
+
+router.get('/', getCompanies);
+
+module.exports = router;
